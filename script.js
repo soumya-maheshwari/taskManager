2cf76fd0bbcec4ef9fa63105583824ba0e6cd03a
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-04-07 17:11:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const input = document.querySelector("input");
+const date = document.getElementById("date");
+const btn = document.querySelector(".addTask > button");
+
+btn.addEventListener("click", addList);
+input.addEventListener("keyup", (e) => {
+  e.keyCode === 13 ? addList(e) : null;
+});
+
+function addList(e) {
+  const notCompleted = document.querySelector(".notCompleted");
+  const Completed = document.querySelector(".Completed");
+
+  const newLi = document.createElement("li");
+  const due_date = document.createElement("li");
+  const checkBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+
+  checkBtn.innerHTML = '<i class="fa fa-check"></i>';
+  delBtn.innerHTML = '<i class="fa fa-trash"></i>';
+
+  if (input.value !== "") {
+    newLi.textContent = input.value;
+    due_date.textContent = date.value;
+    input.value = "";
+    notCompleted.appendChild(newLi);
+    newLi.appendChild(checkBtn);
+    newLi.appendChild(delBtn);
+    newLi.appendChild(due_date);
+  }
+
+  checkBtn.addEventListener("click", function () {
+    const parent = this.parentNode;
+    parent.remove();
+    Completed.appendChild(parent);
+    checkBtn.style.display = "none";
+  });
+
+  delBtn.addEventListener("click", function () {
+    const parent = this.parentNode;
+    parent.remove();
+  });
+}
